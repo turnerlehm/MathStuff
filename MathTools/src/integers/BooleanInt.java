5ca4140249f59a1eba969648dc1525bacb07b75b
turnerlehm <edmfrosty@gmail.com>
2016-06-15 15:24:29 -0700

    Added basic arithmetic operations to BooleanInt

--- MathTools/src/integers/BooleanInt.java
+++ MathTools/src/integers/BooleanInt.java
@@ -1,6 +1,7 @@
 package integers;
 
-/*
+/**
+ * @author Turner C.<!-- --> Lehmbecker
  * Translates a standard 32-bit signed integer into a boolean array for more control over bitwise operations
  * The underlying boolean array can of course be translated back into a standard 32-bit signed integer.
  * All basic arithmetic operations are supported as well as all standard bitwise operations.
@@ -11,10 +12,88 @@ package integers;
  */
 public class BooleanInt 
 {
-
+	private boolean[] bits;
+	public static final BooleanInt ZERO = new BooleanInt();
+	public static final BooleanInt ONE = new BooleanInt(1);
+	public static final BooleanInt TEN = new BooleanInt(10);
+	/**
+	 * Default class constructor
+	 * Characteristically constructs a boolean integer with initial value 0
+	 */
 	public BooleanInt() 
 	{
-		// TODO Auto-generated constructor stub
+		bits = new boolean[32];
 	}
-
+	
+	/**
+	 * Class constructor specifying integer to be converted to/represented as a boolean array
+	 * @param n the 32-bit signed integer to be converted/represented
+	 */
+	public BooleanInt(int n)
+	{
+		translate(n);
+	}
+	
+	/**
+	 * Translates a 32-bit signed integer into a boolean array
+	 * @param n
+	 */
+	private void translate(int n)
+	{
+		bits = new boolean[32];
+		for(int i = 31; i >= 0; i--)
+			bits[i] = (n & (1 << i)) != 0;
+	}
+	
+	/**
+	 * Returns the 32-bit signed integer representation of <code>this</code>
+	 * @return the 32-bit signed integer representation of <code>this</code>
+	 */
+	public int intValue()
+	{
+		int n = 0;
+		for(boolean b : bits)
+			n = (n << 1) | (b ? 1 : 0);
+		return n;
+	}
+	
+	/**
+	 * Multiplies <code>this</code> with the specified integer <code>n</code>
+	 * @param n the integer to be multiplied by
+	 * @return a BooleanInt representing the result of <code>this * n</code>
+	 */
+	public BooleanInt multiply(int n)
+	{
+		return new BooleanInt(this.intValue() * n);
+	}
+	
+	/**
+	 * Multiplies <code>this</code> with the BooleanInt <code>n</code>
+	 * @param n the BooleanInt to be multiplied by 
+	 * @return <code>this * n</code>
+	 */
+	public BooleanInt multiply(BooleanInt n)
+	{
+		return new BooleanInt(this.intValue() * n.intValue());
+	}
+	
+	/**
+	 * Returns a BooleanInt whose value is <code>this + n</code>
+	 * @param n the value to be added
+	 * @return <code>this + n</code>
+	 */
+	public BooleanInt add(int n)
+	{
+		return new BooleanInt(this.intValue() + n);
+	}
+	
+	/**
+	 * Returns a BooleanInt whose value is <code>this + n</code>
+	 * @param n the BooleanInt to be added
+	 * @return <code>this + n</code>
+	 */
+	public BooleanInt add(BooleanInt n)
+	{
+		return new BooleanInt(this.intValue() + n.intValue());
+	}
 }
