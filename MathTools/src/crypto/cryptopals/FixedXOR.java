2a7d67a19de91e11e6d3a90b2bf98959d615cb87
turnerlehm <edmfrosty@gmail.com>
2017-11-16 17:14:03 -0800

    Added Cryptopals fixed XOR class/

---
+++ MathTools/src/crypto/cryptopals/FixedXOR.java
@@ -0,0 +1,40 @@
+package crypto.cryptopals;
+
+import java.math.BigInteger;
+
+public class FixedXOR
+{
+
+    /**
+     * XORs two hex encoded strings of the same length
+     * @param s1 the first hex encoded string
+     * @param s2 the second hex encoded string
+     * @return the hex encoded result of <code>s1 ^ s2</code>
+     * @throws IllegalArgumentException if <code>s1.length() != s2.length()</code>
+     */
+    public String xor(String s1, String s2)
+    {
+        if(s1.length() != s2.length())
+            throw new IllegalArgumentException("Strings must be the same length");
+        byte[] b1 = new BigInteger(s1,16).toByteArray();
+        byte[] b2 = new BigInteger(s2,16).toByteArray();
+        int len = Math.max(b1.length, b2.length);
+        byte[] res = new byte[len];
+        for(int i = 0; i < len; i++)
+            res[i] = (byte)(b1[i % b1.length] ^ b2[i % b2.length]);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < len; i++)
+        {
+            sb.append(Character.forDigit((res[i] >> 4) & 0xF, 16));
+            sb.append(Character.forDigit(res[i] & 0xF, 16));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        FixedXOR fx = new FixedXOR();
+        System.out.println(fx.xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"));
+        System.out.println("matches: " + fx.xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965").equals("746865206b696420646f6e277420706c6179"));
+    }
+}
