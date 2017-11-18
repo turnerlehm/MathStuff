f32aa6be425f189bb408397cf9d1b1c054ec8b69
turnerlehm <edmfrosty@gmail.com>
2017-11-17 23:56:10 -0800

    Added XORCipher

--- MathTools/src/crypto/cryptopals/XORCipher.java
+++ MathTools/src/crypto/cryptopals/XORCipher.java
@@ -1,12 +1,29 @@
 package crypto.cryptopals;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+
 public class XORCipher
 {
 
-    public String encrypt(String key, String plain)
+    public char[] encrypt(String key, String plain)
     {
-        String enc = "";
+        char[] k = key.toCharArray();
+        char[] pt = plain.toCharArray();
+        for(int i = 0; i < pt.length; i++)
+            pt[i] = (char)(pt[i] ^ k[i % k.length]);
+        return pt;
+    }
 
-        return enc;
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String plain = "Burning 'em, if you ain't quick and nimble\n" +
+                "I go crazy when I hear a cymbal";
+        String ct = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        //BigInteger temp = new BigInteger(ct,16);
+        //ct = temp.toString(16);
+        String key =  "ICE";
+        XORCipher xc = new XORCipher();
+        char[] enc = xc.encrypt(key,plain);
+
     }
 }
